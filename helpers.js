67a8898b9063560cb576daf6d9ec14e3f38233d3
Andrew Hill <me@andrewhillcode.com>
2019-06-03 00:38:29 +1000

    Add basic support for Anilist users

--- helpers.js
+++ helpers.js
@@ -1,13 +1,6 @@
-const scraper = require('mal-scraper');
-
-const DEFAULT_MAL_PARAMS = {
-    after: 0,
-    type: "anime"
-};
-
 /**
  * Definition of what the ranks
- * that MAL has corresponds to.
+ * that Anilist has corresponds to.
  */
 const tiers = {
     10: "S",
@@ -21,33 +14,6 @@ const tiers = {
     2: "F",
     1: "F",
     0: "unranked",
-}
-
-/**
- * Attaches metadata to a single anime
- * fetched from MAL
- * @param {MalAnimeResponse} anime 
- * @returns {Anime}
- */
-const transformAnime = (anime) => ({
-    score: anime.score,
-    title: anime.animeTitle,
-    image: anime.animeImagePath.replace('/r/96x136',''),
-    url: `https://myanimelist.net${anime.animeUrl}`,
-    tier: tiers[anime.score]
-})
-
-
-/**
- * Fetches a user's tier list from MAL with
- * added metadata
- * @param {String} user
- * @param {TierListOptions} options
- */
-const fetchTierLists = async (user, { after , type } = DEFAULT_MAL_PARAMS) => {
-    const tiers = await scraper.getWatchListFromUser(user, after, type);
-    // console.log(tiers.map(transformAnime));
-    return tiers.map(transformAnime);
 }
 
 /**
@@ -81,6 +47,6 @@ const tallyAnimeScores = (animes) => {
 }
 
 module.exports = {
-    fetchTierLists,
+    tiers,
     tallyAnimeScores
 }
