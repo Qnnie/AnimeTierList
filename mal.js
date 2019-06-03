6a9130e861896db3010b83167634a513b74c29b3
Xetera <mythologyali@gmail.com>
2019-06-02 21:39:22 -0700

    programmatically splitting requests into batches of 300

--- mal.js
+++ mal.js
@@ -1,46 +1,71 @@
-const helpers = require('./helpers')
-const cheerio = require('cheerio');
-const scraper = require('mal-scraper');
-const request = require('request');
+const helpers = require("./helpers");
+const cheerio = require("cheerio");
+const scraper = require("mal-scraper");
+const fetch = require("node-fetch");
+const { text, flatten } = require("./utils");
+
+const ANIME_COUNT_PER_REQUEST = 300;
+const MAX_ANIMES = 900;
 
 const DEFAULT_MAL_PARAMS = {
-    after: 0,
-    type: "anime",
+  after: 0,
+  type: "anime"
+};
+
+/**
+ * Calculates the amount of requests required to
+ * fetch a user's entire collection
+ * @param {number} totalAnimes
+ */
+const requiredRequestCount = totalAnimes =>
+  Math.ceil(totalAnimes / ANIME_COUNT_PER_REQUEST);
+
+const fetchWatchList = ({ user, type, totalAnimes }) => {
+  const maxAnimes = totalAnimes > MAX_ANIMES ? MAX_ANIMES : totalAnimes;
+  const maxRequestCount = requiredRequestCount(maxAnimes);
+
+  // generate `maxRequestCount` amount of requests
+  // where the offset increases in `ANIME_COUNT_PER_REQUEST`
+  const requests = Array.from({ length: maxRequestCount }, i => {
+    return scraper.getWatchListFromUser(
+      user,
+      ANIME_COUNT_PER_REQUEST * (i + 1),
+      type
+    );
+  });
+
+  // flatten the array of arrays into a single array
+  return Promise.all(requests).then(flatten);
 };
 
 /**
  * ONLY FOR MAL
  * Webscrapes for the number of list entries
- * @param {String} user
- * @returns {listSize} 
+ * @param {string} user
+ * @returns {Promise<number>}
  */
-const fetchUserListSize = (username) => {
-    request({url: `https://myanimelist.net/profile/${username}`}, (err, res, html) => {
-        if (!err && res.statusCode == 200) {
-            const $ = cheerio.load(html);
-            const listSize = $(".stats-data.fl-r .di-ib.fl-r");
-            console.log(listSize.text())
-            return parseInt(listSize.text());
-        } 
-        else {
-            console.log('Could not fetch list size');
-        }
-    });
-}
+const fetchUserListSize = username => {
+  return text(`https://myanimelist.net/profile/${username}`).then(html => {
+    const $ = cheerio.load(html);
+    const listSizeElement = $(".stats-data.fl-r .di-ib.fl-r").first();
+    const listSize = listSizeElement.text().replace(/,/g, "");
+    return Number(listSize);
+  });
+};
 
 /**
  * Attaches metadata to a single anime
  * fetched from MAL
  * @param {MalAnimeResponse} anime
- * @returns {Anime} 
+ * @returns {Anime}
  */
-const transformAnime = (anime) => ({
-    score: anime.score,
-    title: anime.animeTitle,
-    image: anime.animeImagePath.replace('/r/96x136',''),
-    url: `https://myanimelist.net${anime.animeUrl}`,
-    tier: helpers.tiers[anime.score]
-})
+const transformAnime = anime => ({
+  score: anime.score,
+  title: anime.animeTitle,
+  image: anime.animeImagePath.replace("/r/96x136", ""),
+  url: `https://myanimelist.net${anime.animeUrl}`,
+  tier: helpers.tiers[anime.score]
+});
 
 /**
  * Fetches a user's tier list from MAL with
@@ -48,20 +73,14 @@ const transformAnime = (anime) => ({
  * @param {String} user
  * @param {MalListOptions} options
  */
-const fetchTierLists = async (user, { after , type } = DEFAULT_MAL_PARAMS) => {
-    fetchUserListSize(user);
-    //The reason we change after twice is to get list sizes greater than 300.
-    //In this case we are getting up to 900 Entries 
-    const iteration1 = await scraper.getWatchListFromUser(user, after, type);
-    const iteration2 = await scraper.getWatchListFromUser(user, 300, type);
-    const iteration3 = await scraper.getWatchListFromUser(user, 600, type);
-    //Throws all anime objects into the same array.
-    const tiers = iteration1.concat(iteration2).concat(iteration3);
-    //Creates a new array with only the data that transformAnime returns
-    return tiers.map(transformAnime);
-}
+const fetchTierLists = async (user, { after, type } = DEFAULT_MAL_PARAMS) => {
+  const totalAnimes = await fetchUserListSize(user);
+  const animes = await fetchWatchList({ user, type, totalAnimes });
+  //Creates a new array with only the data that transformAnime returns
+  return animes.map(transformAnime);
+};
 
 module.exports = {
-    fetchTierLists,
-    fetchUserListSize
-}
+  fetchTierLists,
+  fetchUserListSize
+};
