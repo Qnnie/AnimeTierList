9a2a4ca11edb779b0c84339ecf8c0824a9256bbe
Xetera <mythologyali@gmail.com>
2019-06-02 21:51:40 -0700

    fixed indentation issue

--- mal.js
+++ mal.js
@@ -13,17 +13,17 @@ const DEFAULT_MAL_PARAMS = {
 };
 
 /**
-* Calculates the amount of requests required to
-* fetch a user's entire collection
-* @param {number} totalAnimes
-*/
+ * Calculates the amount of requests required to
+ * fetch a user's entire collection
+ * @param {number} totalAnimes
+ */
 const requiredRequestCount = totalAnimes =>
-Math.ceil(totalAnimes / ANIME_COUNT_PER_REQUEST);
+    Math.ceil(totalAnimes / ANIME_COUNT_PER_REQUEST);
 
 const fetchWatchList = ({ user, type, totalAnimes }) => {
     const maxAnimes = totalAnimes > MAX_ANIMES ? MAX_ANIMES : totalAnimes;
     const maxRequestCount = requiredRequestCount(maxAnimes);
-    
+
     // generate `maxRequestCount` amount of requests
     // where the offset increases in `ANIME_COUNT_PER_REQUEST`
     const requests = Array.from({ length: maxRequestCount }, i => {
@@ -31,21 +31,21 @@ const fetchWatchList = ({ user, type, totalAnimes }) => {
             user,
             ANIME_COUNT_PER_REQUEST * (i + 1),
             type
-            );
-        });
-        
-        // flatten the array of arrays into a single array
-        return Promise.all(requests).then(flatten);
-    };
-    
-    /**
-    * ONLY FOR MAL
-    * Webscrapes for the number of list entries
-    * @param {string} user
-    * @returns {Promise<number>}
-    */
-    const fetchUserListSize = username => {
-        return text(`https://myanimelist.net/pr2ofile/${username}`).then(html => {
+        );
+    });
+
+    // flatten the array of arrays into a single array
+    return Promise.all(requests).then(flatten);
+};
+
+/**
+ * ONLY FOR MAL
+ * Webscrapes for the number of list entries
+ * @param {string} user
+ * @returns {Promise<number>}
+ */
+const fetchUserListSize = username => {
+    return text(`https://myanimelist.net/pr2ofile/${username}`).then(html => {
         const $ = cheerio.load(html);
         const listSizeElement = $(".stats-data.fl-r .di-ib.fl-r").first();
         const listSize = listSizeElement.text().replace(/,/g, "");
@@ -54,11 +54,11 @@ const fetchWatchList = ({ user, type, totalAnimes }) => {
 };
 
 /**
-* Attaches metadata to a single anime
-* fetched from MAL
-* @param {MalAnimeResponse} anime
-* @returns {Anime}
-*/
+ * Attaches metadata to a single anime
+ * fetched from MAL
+ * @param {MalAnimeResponse} anime
+ * @returns {Anime}
+ */
 const transformAnime = anime => ({
     score: anime.score,
     title: anime.animeTitle,
@@ -68,11 +68,11 @@ const transformAnime = anime => ({
 });
 
 /**
-* Fetches a user's tier list from MAL with
-* added metadata
-* @param {String} user
-* @param {MalListOptions} options
-*/
+ * Fetches a user's tier list from MAL with
+ * added metadata
+ * @param {String} user
+ * @param {MalListOptions} options
+ */
 const fetchTierLists = async (user, { after, type } = DEFAULT_MAL_PARAMS) => {
     const totalAnimes = await fetchUserListSize(user);
     const animes = await fetchWatchList({ user, type, totalAnimes });
