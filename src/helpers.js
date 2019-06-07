a3511dc98b6cbaaf71147d02d1774a940e831fb7
Qnnie <QnnieDev@gmail.com>
2019-06-06 23:15:45 -0700

    cleaning code up

--- src/helpers.js
+++ src/helpers.js
@@ -47,28 +47,19 @@ const getAnimeTier = (score) => {
  * @param {AnimeList[]} animes 
  */
 const tallyAnimeScores = (animes) => {
-    // When we tally these scores, the animes that don't have
-    // any rankings, like no S tier, will automatically be dropped because they 
-    // will never be attempted to get converted to an array of animes
-    // (because they don't exist)
     return animes.reduce((state, anime) => {
-        const { tier } = anime; //Grabs anime.tier 
-        // the current animes that are in the object being built
-        const currentTier = state[tier];
-        // the updated array, empty if we never added an anime of this tier before
-        const newAnimeState = currentTier || [];
+        const { tier } = anime; //Grabs anime.tier
 
-        if (currentTier || []) {
-            newAnimeState.push(anime)
-        }
+        // the updated array, empty if we never added an anime of this tier before
+        const newTier = state[tier] || []; 
+        
+        newTier.push(anime);
+        
         return {
-            // spread the previous state in to include all previous data
             ...state,
-            // set the new tier to equal every anime from before
-            // plus the one we just pushed on the updated state
-            [tier]: newAnimeState
+            [tier]: newTier
         }
-    }, {} /* initial state */);
+    }, {});
 }
 
 module.exports = {
